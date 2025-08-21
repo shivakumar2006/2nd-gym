6376a05c12979930e5155cacef06d2d221a6d39c
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-21 09:59:26 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,17 +48,17 @@ const About = ({ aboutRef }) => {
         </div>
 
         <div className='text-white'>
-            <p className='text-white text-4xl mt-20 ml-20'><span className='text-[#C7FF39] font-extrabold text-6xl'>++</span> WHY CHOOSE US</p>
+            <p className='text-white text-3xl md:text-4xl mt-20 md:ml-20 ml-12'><span className='text-[#C7FF39] font-extrabold text-6xl'>++</span> WHY CHOOSE US</p>
         </div>
-        <div className='w-full mt-10 flex flex-row justify-around items-center'>
-            <p className='text-white font-extrabold text-5xl mr-80'>PUSH YOUR <span className='text-[#C7FF39]'>LIMIT <br />FORWARD</span></p>
-            <p className='text-[14px] text-white'>Train with the best experts in Bodybuilding Field. Your <br /> 
+        <div className='w-full mt-10 flex flex-col md:flex-row justify-around items-center'>
+            <p className='text-white font-extrabold text-center text-4xl md:text-5xl md:mr-80'>PUSH YOUR <span className='text-[#C7FF39]'>LIMIT <br />FORWARD</span></p>
+            <p className='md:text-[14px] text-white text-[12px]'>Train with the best experts in Bodybuilding Field. Your <br /> 
                 Results will speaks to themselves. Try the Latest Exercising <br />
                 Trends. Innovative Equipment And Special Nutrition Plans.
             </p>
         </div>
-        <div className='w-full mt-20 flex justify-center items-center'>
-            <div className='w-320 flex flex-wrap justify-around items-center'>
+        <div className='w-full mt-20 flex justify-center items-center pb-15 md:pb-0'>
+            <div className='w-320 flex flex-wrap justify-around items-center gap-5 md:gap-0 mt-[-50px] md:mt-0'>
                 {information?.map((item, index) => (
                     <div key={index} className='w-63 h-53 flex flex-col text-white bg-black/20 rounded-2xl transition-transform duration-300 hover:scale-103 hover:bg-[#FFFFFA] hover:text-black group shadow-xl'>
                     <div className='w-15 h-15 mt-5 ml-4 rounded-full bg-black/30 flex justify-center items-center group-hover:bg-black'>
